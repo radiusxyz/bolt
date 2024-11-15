469777788a0a9af6d30e7b06b257b74f5734c992
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-11-15 16:45:52 +0100

    chore: handle edge case with cargo

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -18,17 +18,13 @@ if ! rustup target list | grep -q $TARGET_ARCH; then
     rustup target add $TARGET_ARCH
 fi
 
-# 2. get the native architecture name (first part of the target triple)
-NATIVE_ARCH=$(uname -m)
-if [[ "$(uname)" == "Darwin" && "$NATIVE_ARCH" == "arm64" ]]; then
-    NATIVE_ARCH="aarch64"
-fi
-echo "Native architecture: $NATIVE_ARCH"
-
-# 3. build the binary:
-# - if the target is the same as the native architecture, build with cargo
-# - otherwise, build with cargo-zigbuild
-if [[ "$TARGE_ARCH" == "$NATIVE_ARCH-unknown-linux-gnu" ]]; then
+# 2. build the binary.
+#
+# Note: everything so far can be built with zigbuild, except for bolt-boost 
+# targeting x86_64-unknown-linux-gnu. As this is still unresolved, we handle
+# this case separately by building the binary with cargo instead. This is less 
+# than ideal, but it works and it's simple
+if [ $PACKAGE == "bolt-boost" && $TARGET_ARCH == "x86_64-unknown-linux-gnu" ]; then
     echo "Building binary with cargo"
     (cd $PACKAGE && cargo build --target $TARGE_ARCH --release)
 else
@@ -36,7 +32,7 @@ else
     (cd $PACKAGE && cargo zigbuild --target $TARGET_ARCH --profile release)
 fi
 
-# 4. copy the binary to the output directory
+# 3. copy the binary to the output directory
 mkdir -p dist/bin/$OUT_DIR
 cp $PACKAGE/target/$TARGET_ARCH/release/$PACKAGE dist/bin/$OUT_DIR/$PACKAGE
 
