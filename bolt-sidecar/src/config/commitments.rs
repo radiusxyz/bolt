05a38e39e1dfefce6fcb53d1ff4b7a875efff7b7
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-12-11 11:27:30 +0100

    test(sidecar): rpc url parsing

--- bolt-sidecar/src/config/commitments.rs
+++ bolt-sidecar/src/config/commitments.rs
@@ -22,10 +22,43 @@ pub struct CommitmentOpts {
     pub port: Option<u16>,
     /// Comma-separated list of allowed RPC addresses to subscribe via websocket to receive
     /// incoming commitments requests.
-    #[clap(long, env = "BOLT_SIDECAR_FIREWALL_RPC", conflicts_with("commitments_port"))]
+    #[clap(
+        long,
+        env = "BOLT_SIDECAR_FIREWALL_RPC",
+        value_delimiter = ',',
+        conflicts_with("commitments_port")
+    )]
     pub firewall_rpc_list: Option<Vec<Url>>,
     /// Secret ECDSA key used to sign commitment messages on behalf of your validators.
     /// This MUST be set to the private key of your operator address registered in a restaking protocol.
     #[clap(long, env = "BOLT_SIDECAR_COMMITMENT_PRIVATE_KEY")]
     pub operator_private_key: EcdsaSecretKeyWrapper,
+}
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+    use reqwest::Url;
+
+    #[derive(Parser)]
+    struct TestCommitmentOpts {
+        /// Matches [CommitmentOpts.firewall_rpc_list]
+        #[clap(long, env = "RPC", value_delimiter = ',')]
+        firewall_rpc_list: Option<Vec<Url>>,
+        /// Captures extra arguments like the name of the test being run
+        #[clap(allow_hyphen_values = true, trailing_var_arg = true)]
+        pub extra_args: Option<Vec<String>>,
+    }
+
+    #[test]
+    fn test_parse_rpc_urls() {
+        let url_0 = "http://localhost:8080";
+        let url_1 = "http://localhost:8081";
+        let urls: Vec<Url> = [url_0, url_1].iter().map(|u| u.parse().expect("valid url")).collect();
+
+        std::env::set_var("RPC", format!("{},{}", url_0, url_1));
+        let opts = TestCommitmentOpts::parse();
+
+        assert_eq!(opts.firewall_rpc_list, Some(urls));
+    }
 }
