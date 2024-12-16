f511fc4f16b8c00ed37791d7c4af8cc2321ab434
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-12-16 10:53:19 +0100

    feat(sidecar): add git commit hash to version tag

--- bolt-sidecar/src/common/mod.rs
+++ bolt-sidecar/src/common/mod.rs
@@ -1,11 +1,19 @@
+use lazy_static::lazy_static;
+
 /// Utilities for retrying a future with backoff.
 pub mod backoff;
+
 /// A hash map-like bounded data structure with an additional scoring mechanism.
 pub mod score_cache;
+
 /// Secret key types wrappers for BLS, ECDSA and JWT.
 pub mod secrets;
+
 /// Utility functions for working with transactions.
 pub mod transactions;
 
-/// The version of the Bolt sidecar binary.
-pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+lazy_static! {
+    /// The version of the Bolt sidecar binary.
+    pub static ref BOLT_SIDECAR_VERSION: String =
+        format!("v{}-{}", env!("CARGO_PKG_VERSION"), crate::built_info::GIT_COMMIT_HASH_SHORT.unwrap_or("unknown"));
+}
